ac751d665c17e98a4da7e6ccee8afd2f082845d5
alaoomotoyosi <alaoomotoyosi@gmail.com>
2022-09-09 21:13:58 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the function
+*
+*Description: using the main function
+*this program prints "Programming is like builing a multilingual puzzle
+* Return 0
+*/
+int main (void)
+{
+         print ("with proper, but the outcome is a piece of art,\n");
+         returnb (0);
+}
