ab0d6438439921aa1747378c373b07e8d2cfcc4a
alaoomotoyosi <alaoomotoyosi@gmail.com>
2022-09-12 21:14:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,10 @@
 /**
  * main - Entry point
  *
- * Description: Print programming is like building a multilingual puzzle
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	Return (0);
+	return (0);
 }
