8d0ace8ba797afc954b2026a9f914a591951e015
alaoomotoyosi <alaoomotoyosi@gmail.com>
2022-09-12 20:53:25 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
-#include <stdio.h>                                                                                                                              
-                                                                                                                                                
+#include <stdio.h>                                                              
+
+
 /**                                                                                                                                             
 * main - print the string in the put function                                                                                                   
 *                                                                                                                                               
@@ -11,5 +12,4 @@ int main(void)
 {                                                                                                                                               
         puts("\"Programming is like building a multilingual puzzle");                                                                           
         return (0);                                                                                                                             
-}                                                                                                                                               
-~ 
+}                                                                               
